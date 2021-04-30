89c5a40ed0ae8c1ca68530a81dfe3a3314c7e3af
Aline Alves <alves.aline2@yahoo.com.br>
2021-04-30 15:17:47 -0300

    adicionando os novos livros e retirando os campos de data de inserção

--- Exercicios/4. array.push()/script.js
+++ Exercicios/4. array.push()/script.js
@@ -1,1 +1,3 @@
 const listagemDeFrutas = [ "Uva", "Banana", "Manga", "Cajá", "Pinha", "Maçã", "Melão"];
+listagemDeFrutas.push("Tangerina", "Morango", "Mangaba", "Jabuticaba", "Graviola");
+console.log(listagemDeFrutas);
--- Exercicios/9. array.splice()/script.js
+++ Exercicios/9. array.splice()/script.js
@@ -1,4 +1,8 @@
 const countryList = [ "Argentina","Armenia","Australia","Azerbaijan","Bahamas","Brazil","Burkina Faso", "Costa Rica","Mauritania","St Vincent","Uganda","United Arab Emirates","Uruguay","Uzbekistan","Venezuela"];
+
+countryList.splice(0, 3, "Italia", "Turquia", "Mexico")
+console.log(countryList);
+
 const usuarios = [
     {user:234, name: 'Marcia', idade:40 },
     {user:235, name: 'Lorena', idade:20 },
@@ -7,3 +11,7 @@ const usuarios = [
     {user:238, name: 'Isis', idade:34 },
     {user:239, name: 'Pietra', idade:23 }
 ]
+usuarios.splice(3,1, {
+    user:240, name: 'Aline', idade:100
+});
+console.log(usuarios);
